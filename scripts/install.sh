0c5f787d105ae0bd56813b164126b6233d9aaa89
Jeff Gu Kang <kmg@monymony.co>
2026-01-11 22:02:04 +0900

    chore: bump version to 1.3.0

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="1.2.0"
+VERSION="1.3.0"
 REPO_URL="https://raw.githubusercontent.com/JeffGuKang/english-ssam/main"
 RULE_FILE="ENGLISH_SSAM.md"
 VERSION_CHECK_FILE="$HOME/.english-ssam-last-check"
